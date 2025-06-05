d2e49d63105f3f1d306f86575fdefafe1c823817
AdnanAliTyping <9229009x@gmail.com>
2025-06-06 00:51:26 +0400

    Create index.ts

---
+++ src/schemas/index.ts
@@ -0,0 +1,12 @@
+const schemas = [
+  {
+    "@context": "https://schema.org",
+    "@type": "WebPage",
+    "name": "DQAA Eid Al-Adha Greeting",
+    "url": "https://darul-quran.com/eid-greeting",
+    "description": "A beautiful Eid Al-Adha digital greeting from DQAA. Share the joy with your loved ones.",
+    "image": "https://darul-quran.com/eid-card-preview.jpg",
+  },
+];
+
+export default schemas;
